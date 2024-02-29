0c23127009c87afbc7c6953cc0011231269067eb
Sahil Rajesh Shriwardhankar <sahiilshriwardhankar@gmail.com>
2024-03-01 00:17:38 +0530

    Updated code

---
+++ pages/AI_Assistant_Chat.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("Under Development")
+    st.write("AI Chat Assistant currently under development. Please check back later!")
+
+if __name__ == "__main__":
+    main()
---
+++ utils/constants.py
@@ -0,0 +1,88 @@
+chat = {
+    "TITLE1": "About Me",
+    "TITLE2": "AI Chat",
+    "menu_v": {
+   "container": {"background-color": "#a7c5ff"},
+   "icon": {"color": "white", "font-size": "25px"}, 
+   "nav-link": {"font-size": "25px", "text-align": "center", "color": "white", "margin":"0px", "--hover-color": "#1b97ff"},
+   "nav-link-selected": {"background-color": "#1b97ff"}
+    },
+    "menu_h": {
+        "container": {"padding": "0px",
+                      "display": "grid",
+                      "margin": "0!important",
+                      "background-color": "#212121"
+                      },
+        "icon": {"color": "#bd93f9", "font-size": "14px"},
+        "nav-link": {
+            "font-size": "14px",
+            "text-align": "center",
+            "margin": "auto",
+            "background-color": "#262626",
+            "height": "30px",
+            "width": "13rem",
+            "color": "#edff85",
+            "border-radius": "5px"
+        },
+        "nav-link-selected": {
+            "background-color": "#212121",
+            "font-weight": "300",
+            "color": "#f7f8f2",
+            "border": "1px solid #e55381"
+        }
+    }
+}
+
+info = {
+   "Pronoun": "he", 
+   "Subject": "i", 
+   "Name": "Sahiil",
+   "Full_Name":"Sahiil Shriwardhankar", 
+   "Intro": "A CSE student, passionate about LLMs and tech trends, diving deep into the world of AI!",
+   "About":"Hey there, I'm Sahiil, a final-year student. Take a peek into my portfolio to discover the projects I've built and the certifications I've earned. Let's dive into the world of innovation together!",
+   "City":"Roha",
+   "Resume": "https://resume.io/r/OsIcBraPd",
+   "Email": "notsointresting@protonmail.com" 
+}
+
+projects = [
+        {
+            "title": "NewsBot",
+            "description": "Telegram bot, News API, Technology news, Telegram chat",
+            "image_url": "https://i.ibb.co/jW6tgMC/telegram-news-bot-saying-Hi.png",
+            "link": "https://github.com/notsointresting/NewsBot"
+        },
+        {
+            "title": "Birthday and Festival Wishing Service",
+            "description": "Python-based, MySQL database, Personalized greetings, Birthday wishes, Festival wishes",
+            "image_url": "https://i.ibb.co/3YQ4zkQ/Birthday-and-Festival-Wishing-Service-Bot.png",
+            "link": "https://github.com/notsointresting/SoftwareEngineering/tree/main/SoftwareEngineering/Lab%20Assesment"
+        },      
+        {
+            "title": "Event Management Project",
+            "description": "Python integration, Database management, Event management, Data accessibility",
+            "image_url": "https://i.ibb.co/Cz0k1Hf/Event-Management.png",
+            "link": "https://github.com/notsointresting/ACES-Event-Manager"
+        },
+        {
+            "title": "IoT Based Air Purifier",
+            "description": "IoT project, Arduino coding, Air purifier functionality, Enhanced air quality",
+            "image_url": "https://i.ibb.co/3TGV6Y6/Io-T-Based-Air-Purifier-using-Arduino.png",
+            "link": "https://github.com/notsointresting/Air_Purifier"
+        },
+        {
+            "title": "Crypto Trading Algorithm",
+            "description": "Cryptocurrency tracking, Market analysis, Trading strategy implementation, Algorithm performance assessment",
+            "image_url": "https://i.ibb.co/qJtsFpg/Crypto-Trading-Algorithm.png",
+            "link": "https://github.com/notsointresting/Crypto-Trading-Algo"
+        },
+        {
+            "title": "Stock Market Bot",
+            "description": "Python Flask application, Twilio integration, WhatsApp messaging, MarketStack API, Stock price updates",
+            "image_url": "https://i.ibb.co/ZxQfw9j/bot-for-stock-market.png",
+            "link": "https://github.com/notsointresting/Stock-Market-Bot"
+        }                  
+    ]
+
+
+

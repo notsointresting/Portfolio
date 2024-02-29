8f643f27d0c9a9979cf366629d1be585213a5d6f
Sahil Rajesh Shriwardhankar <sahiilshriwardhankar@gmail.com>
2024-03-01 00:18:50 +0530

    deleted prev cod

--- main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def main():
-    st.title("Under Development")
-    st.write("This site is currently under development. Please check back later!")
-
-if __name__ == "__main__":
-    main()
